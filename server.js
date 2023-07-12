085eb4d5cd5d733fb306a571bdfaa80269286cc4
AudunTA <aud-and2@online.no>
2023-07-12 22:44:13 +0200

    new changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const Product = requred("./models/productModel");
+const Product = require("./models/productModel");
 app.use(express.json());
 
 //routes
