8039a9c411decd6f0dabdb14a3f5a8e017f6390e
AudunTA <aud-and2@online.no>
2023-07-12 15:20:18 +0200

    product model

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const Product = requred("./models/productModel");
+app.use(express.json());
 
+//routes
 app.get("/", (req, res) => {
   res.send("Hello api");
 });
 
+app.post("/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: e.message });
+  }
+});
+
+mongoose.set("strictQuery", false);
 mongoose
   .connect(
     "mongodb+srv://admin:470555184@linkupapi.xdq7mj8.mongodb.net/LinkUp-API?retryWrites=true&w=majority"
